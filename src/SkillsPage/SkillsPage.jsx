42a7edfce0e06ce2a150c48815485e1c66140447
nicchavez <nicolaschavez530@gmail.com>
2024-03-10 23:47:25 -0300

    Doing project page

--- src/SkillsPage/SkillsPage.jsx
+++ src/SkillsPage/SkillsPage.jsx
@@ -2,13 +2,15 @@ import Contact from "../Home/Contact/Contact";
 import Footer from "../Footer/Footer";
 import Header from "../Header/Header";
 import { Meteors } from "../ui/meteors";
+import { Link, NavLink, useHref } from "react-router-dom";
+import { LinkIcon } from "@nextui-org/react";
 
 
 
-export function SkillCard({title, shortDescription, image}) {
+export function SkillCard({title, shortDescription, link, image}) {
   return (
-    <div className="mx-8 my-8 flex">
-      <div className="w-full relative h-74">
+    <div className="mx-8 my-8 flex sm:justify-center">
+      <div className="w-full  relative h-74 sm:w-80" >
         <div className="absolute inset-0 h-full w-full bg-gradient-to-r from-blue-500 to-teal-500 transform scale-[0.80] bg-red-500 rounded-full blur-3xl" />
         <div className="relative shadow-xl bg-gray-900 border border-gray-800 px-4 py-8 h-full overflow-hidden rounded-2xl flex flex-col justify-end items-start">
           <div className="h-5 w-5 rounded-full border flex items-center justify-center mb-2 border-gray-500">
@@ -38,7 +40,7 @@ export function SkillCard({title, shortDescription, image}) {
           </p>
 
           <button className="border px-4 py-1 rounded-lg border-gray-500 text-gray-300 self-end">
-            Explore
+            <Link to={link} target="blank">Explore</Link>
           </button>
 
         </div>
@@ -51,17 +53,17 @@ export default function SkillsPage() {
   return (
     <>
     <div className="mx-4 mb-16 xl:mx-12 md:mx-12 ">
-      <div className="grid grid-cols-1 xl:grid-cols-5 md:grid-cols-3 ">
-        <SkillCard title="HTML" shortDescription={"Hyper Text Marked Language."} image={"src/assets/htmlLogo.svg"}/>
-        <SkillCard title="CSS" shortDescription={"Cascade Style Sheet, define the design of html file. "} image={"src/assets/cssLogo.svg"}/>
-        <SkillCard title="JavaScript" shortDescription={"The Word Wide Web programming language."} image={"src/assets/jsLogo.svg"}/>
-        <SkillCard title="Tailwind" shortDescription={"The CSS framework to create attractive designs."} image={"src/assets/tailwindLogo.svg"}/>
-        <SkillCard title="React" shortDescription={"The most popular Front-end libarary."} image={"src/assets/reactLogo.svg"}/>
-        <SkillCard title="Next JS" shortDescription={"The most popular Front-end libarary."} image={"src/assets/nextjsLogo.svg"}/>
-        <SkillCard title="Python" shortDescription={"The all in one of programming language."} image={"src/assets/pythonLogo.svg"}/>
-        <SkillCard title="Django" shortDescription={"Back-end framwork based in MVC model."} image={"src/assets/djangoLogo2.svg"}/>
-        <SkillCard title="SQL" shortDescription={"Relational data base mos used."} image={"src/assets/sqlLogo2.svg"}/>
-        <SkillCard title="MongoDB" shortDescription={"The most popular Front-end libarary."} image={"src/assets/mongodbLogo.svg"}/>
+      <div className="grid grid-cols-1 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 ">
+        <SkillCard title="HTML" shortDescription={"Hyper Text Marked Language."} link="https://developer.mozilla.org/en-US/docs/Web/HTML" image={"src/assets/htmlLogo.svg"}/>
+        <SkillCard title="CSS" shortDescription={"Cascade Style Sheet, define the design of html file. "} link={"https://developer.mozilla.org/en-US/docs/Web/CSS"} image={"src/assets/cssLogo.svg"}/>
+        <SkillCard title="JavaScript" shortDescription={"The Word Wide Web programming language."} link={"https://developer.mozilla.org/en-US/docs/Web/JavaScript"} image={"src/assets/jsLogo.svg"}/>
+        <SkillCard title="Tailwind" shortDescription={"The CSS framework to create attractive designs."} link={"https://tailwindcss.com/"} image={"src/assets/tailwindLogo.svg"}/>
+        <SkillCard title="React" shortDescription={"The most popular Front-end libarary."} link={"https://react.dev/"} image={"src/assets/reactLogo.svg"}/>
+        <SkillCard title="Next JS" shortDescription={"React framework, organize your path scheme."} link={"https://nextjs.org/"} image={"src/assets/nextjsLogo.svg"}/>
+        <SkillCard title="Python" shortDescription={"The all in one of programming languages."} link={"https://www.python.org/"} image={"src/assets/pythonLogo.svg"}/>
+        <SkillCard title="Django" shortDescription={"Back-end framwork based in MVC model."} link={"https://www.djangoproject.com/"} image={"src/assets/djangoLogo2.svg"}/>
+        <SkillCard title="SQL" shortDescription={"Relational data base most used."} link={"https://www.w3schools.com/sql/sql_intro.asp"} image={"src/assets/sqlLogo2.svg"}/>
+        <SkillCard title="MongoDB" shortDescription={"No relational data base most used."} link={"https://www.mongodb.com/"} image={"src/assets/mongodbLogo.svg"}/>
       </div>
     </div>
 
